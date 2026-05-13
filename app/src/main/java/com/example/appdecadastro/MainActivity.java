475278ae0d1b6577ff47f488f196ac41cc0cc098
miragenn <miragen200@gmail.com>
2026-05-13 17:37:09 -0300

    Salvando alterações locais

--- app/src/main/java/com/example/appdecadastro/MainActivity.java
+++ app/src/main/java/com/example/appdecadastro/MainActivity.java
@@ -35,13 +35,11 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
+
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.menu_sair) {
             return true;
         }
         return super.onOptionsItemSelected(item);
@@ -203,4 +201,6 @@ public class MainActivity extends AppCompatActivity {
 
         cursor.close();
     }
+
+
 }
